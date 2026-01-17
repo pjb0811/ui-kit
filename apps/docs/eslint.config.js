4ad68443c129ef69163a8354acde4bec3111854d
pjb0811 <pjb0811@gmail.com>
2026-01-17 14:00:19 +0900

    🔧 chore(docs): update ESLint configuration and dependencies
    - Simplify ESLint configuration by removing Storybook plugin
    - Update lint script to use ESLint directly
    - Upgrade Storybook and ESLint plugin dependencies

--- apps/docs/eslint.config.js
+++ apps/docs/eslint.config.js
@@ -1,18 +1,4 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from 'eslint-plugin-storybook';
-
 import { nextJsConfig } from '@repo/eslint-config/next-js';
 
 /** @type {import("eslint").Linter.Config} */
-export default [
-  ...nextJsConfig,
-  {
-    plugins: {
-      storybook,
-    },
-    rules: {
-      ...storybook.configs.recommended.rules,
-    },
-    files: ['**/*.stories.@(js|jsx|ts|tsx)'],
-  },
-];
+export default nextJsConfig;
